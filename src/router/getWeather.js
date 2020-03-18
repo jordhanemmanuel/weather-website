5e594d36b3746a3f9dabc8623b2a09915c7e0359
jordhanemmanuel@gmail.com <jordhanemmanuel@gmail.com>
2020-03-18 10:34:01 -0300

    Ajuste na requisicao

--- src/router/getWeather.js
+++ src/router/getWeather.js
@@ -4,42 +4,45 @@ const geocode = require('../utils/geocode')
 const forecast = require('../utils/forecast')
 
 router.get('/api/get', (req,ret) => {
-    if (!req.query.address){
+
+    if (!req.body.endereco){
         return ret.send({
-            error: 'Erro: Não foi informado nenhum endereço.'
+            error: 'Erro: Não foi informado nenhum endereço.',
+            errorID: 1
         })
     }
     
-    geocode.getGeocode(req.query.address, (err, res) => {
+    geocode.getGeocode(req.body.endereco, (err, res) => {
         if (!res)
-            return ret.send({error:err, log: 'Linha 57'})
+            return ret.send({error:err, errorID: 2})
             
         const { longitude, latitude, local, geoLocal } = res
 
         forecast.getPrevisao(geoLocal, (err, res) => {
             if (!res)
-                return ret.send({error:err, log: 'Linha 62', geoLocal})
-
-            ret.send({
-                address: req.query.adress,
-                local,
-                geoLocation: {
-                    longitude,
-                    latitude,
-                },
-                clima: {
-                    celsius: res.celsius,
-                    probchuva: res.probChuva,
-                    resumo: res.resumo,
-                    tempMax: res.tempMax,
-                    tempMin: res.tempMin,
-                    infoResumo: res.infoResumo,
-                    infoChuva: res.infoChuva,
-                    infoTemp: res.infoTemp
-                }
-            })
+                return ret.send({error:err, geoLocal, errorID: 3})    
+        })
+        
+        ret.send({
+            address: req.body.endereco,
+            local,
+            geoLocation: {
+                longitude,
+                latitude,
+            },
+            clima: {
+                celsius: res.celsius,
+                probchuva: res.probChuva,
+                resumo: res.resumo,
+                tempMax: res.tempMax,
+                tempMin: res.tempMin,
+                infoResumo: res.infoResumo,
+                infoChuva: res.infoChuva,
+                infoTemp: res.infoTemp
+            }
         })
     })
 })
-
+    
 module.exports = router
+
